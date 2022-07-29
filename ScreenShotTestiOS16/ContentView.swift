4664bb20df31d74158564c98b4403701076410ff
Dong487 <Dsheng7777777@gmail.com>
2022-07-29 15:41:39 +0800

    Initial Commit

---
+++ ScreenShotTestiOS16/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ScreenShotTestiOS16
+//
+//  Created by DONG SHENG on 2022/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScreenShotTestiOS16/ScreenShotTestiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotTestiOS16App.swift
+//  ScreenShotTestiOS16
+//
+//  Created by DONG SHENG on 2022/7/29.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotTestiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
